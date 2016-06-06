716b5cbf1fbee989df7c5ac9ba44bc8f20a82ea0
David Dunlop <david.dunlop@rackspace.com>
2016-06-06 11:58:41 -0700

    Add a timeout to the api request

--- config/settings.js
+++ config/settings.js
@@ -6,5 +6,6 @@ module.exports = {
     "url": process.env.HEALTH_APP_URL || "https://intelligence.rackspace.com/health",
     "username": process.env.HEALTH_APP_USER || "",
     "password": process.env.HEALTH_APP_PASS || ""
-  }
+  },
+  "timeout": 20000
 }
